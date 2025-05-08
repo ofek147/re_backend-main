e3212f810ba17a4f73c9a138c642ca156eaad1c6
Ofek Keinan <ofek.keinan@gmail.com>
2025-05-08 10:59:41 +0300

    TRY HTTPS

--- src/server.ts
+++ src/server.ts
@@ -1,65 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose"; // Import Mongoose
-// import projectRoutes from "./routes/projects";
-// import adminRoutes from "./routes/admin";
-// import leadRoutes from "./routes/leads";
-
-// const dotenv = require("dotenv");
-// // Load environment variables
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 9999;
-// const MONGODB_URI = process.env.MONGODB_URI; // Add your MongoDB URI to .env
-
-// if (!MONGODB_URI) {
-//   console.error("MONGODB_URI environment variable is not defined.");
-//   process.exit(1);
-// }
-
-// // Middleware
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Connect to MongoDB
-// mongoose
-//   .connect(MONGODB_URI)
-//   .then(() => console.log("Connected to MongoDB"))
-//   .catch((err) => {
-//     console.error("Could not connect to MongoDB", err);
-//     process.exit(1); // Exit the process if connection fails
-//   });
-
-// // Routes
-// app.use(projectRoutes);
-// app.use(adminRoutes);
-// app.use(leadRoutes);
-
-// // Health check route
-// app.get("/health", (req, res) => {
-//   res.status(200).json({ status: "ok" });
-// });
-
-// // Global error handler
-// app.use(
-//   (
-//     err: any,
-//     req: express.Request,
-//     res: express.Response,
-//     next: express.NextFunction
-//   ) => {
-//     console.error(err.stack);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// );
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -74,8 +12,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 9999; // פורט ברירת מחדל אם לא מוגדר אחרת
-const HTTP_PORT = 80; // פורט עבור הפניית HTTP ל-HTTPS
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const DOMAIN = "re-backend.xyz"; // שם הדומיין שלך
@@ -142,8 +78,3 @@ httpsServer.listen(3333, () => {
   // האזנה על פורט 3333 עבור HTTPS (מאחורי Nginx)
   console.log(`HTTPS server is running on port 3333`);
 });
-
-// אם את כבר לא צריכה להאזין על הפורט הישן (9999), את יכולה להסיר את השורה הבאה:
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
