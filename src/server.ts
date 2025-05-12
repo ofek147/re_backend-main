92911e634382969e5737f8f6ae6e971bda550a8d
Ofek Keinan <ofek.keinan@gmail.com>
2025-05-12 12:10:19 +0300

    HTTPS FIX FOR ADMIN_DASHBOARD

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ if (!MONGODB_URI) {
 // Middleware
 app.use(
   cors({
-    origin: ["https://re-backend.xyz", "https://api.re-backend.xyz"],
+    origin: ["https://re-backend.xyz", "https://api.re-backend.xyz", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
